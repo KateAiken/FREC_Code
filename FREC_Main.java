d2f3899cbf597d9cdffc693d67bf977a97e7b8e3
KateAiken <kate_aiken@icloud.com>
2025-01-01 13:44:59 -0500

    Add files via upload

--- FREC_Main.java
+++ FREC_Main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 public class FREC_Main{
     public static void main(String[] args) {
+
         FREC_Class quiz = new FREC_Class();
-        System.out.println("What FREC are you?");
         Scanner input = new Scanner(System.in);
+
+        System.out.println("What FREC are you? Take the quiz to find out!");
         String command = quiz.getOption(input);
+
         while(!command.equals("q")){
             if (command.equals("i")){
                 quiz.instructions();
@@ -16,6 +20,7 @@ public class FREC_Main{
             }
             else if (command.equals("p")){
                 String playerAnswer[] = new String[10];
+                
                 for (int quizNum = 0; quizNum < 10; quizNum ++){
                     quiz.printQuestion(quizNum);
                     playerAnswer[quizNum] = quiz.getAns(input);
